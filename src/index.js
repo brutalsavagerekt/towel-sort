a16673c3b373346f7d08362158a095f3971a06e4
brutalsavagerekt <just.bondarchuk@gmail.com>
2023-08-17 16:48:43 +0300

    feat:create towelSort function to sort matrix array

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let output = [];
+  // делаем проверку на пустой массив
+  if (matrix == undefined) {
+    return []
+  } else {
+    for (let i = 0; i < matrix.length; i++) {
+      // перебираем массив с массивами
+      // если у нас индексы 0,2,4,6...оставляем как есть
+      if (i % 2 === 0) {
+        // добавляем в общий массив
+        output = output.concat(matrix[i])
+        // если индексы 1,3,5...
+      } else {
+        // добавляем в массив, только сделав реверс
+        output = output.concat(matrix[i].reverse())
+      }
+    }
+  }
+  
+
+  return output
 }
